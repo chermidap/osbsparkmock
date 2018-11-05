0bb68689b18d7359907d70ca10fa77c93d6767c9
cristobal.hermida <cristobal.hermida@intelygenz.com>
2018-11-05 14:45:06 +0100

    add abonos service methods

--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -1,10 +1,24 @@
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class Service {
 
     public static void main( String[] args) {
         get("/posts", (req, res) -> {
             return "Hello Sparkingly World!";
+        });
+
+        post("/getPrecioAbono",(request, response) -> {
+            return "getPrecioAbono!";
+        });
+        post("/costeAltaAbono",(request, response) -> {
+            return "costeAltaAbono!";
+        });
+        post("/preAltaAbono",(request, response) -> {
+            return "preAltaAbono!";
+        });
+        post("/datosCompletos",(request, response) -> {
+            return "datosCompletos!";
         });
     }
 }
